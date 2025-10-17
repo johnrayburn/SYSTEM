1fe253e8f58ea6a5049bc121d00b62fd9669b5b8
John Rayburn <john1rayburn@gmail.com>
2025-10-17 16:20:49 -0400

    Added Lazyvim, changed filename

---
+++ Neovim-Lazyvim_INSTALL.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+pushd /opt
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+rm -rf /opt/nvim
+tar -C /opt -xzf nvim-linux64.tar.gz
+ln -sf /opt/nvim-linux64/bin/nvim /usr/local/bin/nvim
+popd
+
+# required
+mv ~/.config/nvim{,.bak}
+
+# optional but recommended
+mv ~/.local/share/nvim{,.bak}
+mv ~/.local/state/nvim{,.bak}
+mv ~/.cache/nvim{,.bak}
+
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+# remove PROJECT git folder to be able to add it to PERSONAL git repo 
+rm -rf ~/.config/nvim/.git
+
+nvim
