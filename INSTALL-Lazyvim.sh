97e75d6c174a0a5e24fd77f3652ff40110d0ff90
John Rayburn <john1rayburn@gmail.com>
2025-11-02 10:15:00 -0500

    Made Executable

--- INSTALL-Lazyvim.sh
+++ INSTALL-Lazyvim.sh
@@ -13,4 +13,9 @@ git clone https://github.com/LazyVim/starter ~/.config/nvim
 # remove PROJECT git folder to be able to add it to PERSONAL git repo 
 rm -rf ~/.config/nvim/.git
 
+ln -sf {./personal,~/config}/nvim/lua/config/keymaps.lua
+ln -sf {./personal,~/config}/nvim/lua/config/options.lua
+ln -sf {./personal,~/config}/nvim/lua/plugins/colorschemes.lua
+ln -sf {./personal,~/config}/nvim/lua/plugins/disabled.lua
+
 nvim
