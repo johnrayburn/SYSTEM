8115eeb2412a6235245f3cd75bc138250e010866
John Rayburn <john1rayburn@gmail.com>
2025-11-04 04:10:45 +0000

    Tue Nov  4 04:10:45 AM UTC 2025

--- INSTALL-Neovim.sh
+++ INSTALL-Neovim.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 pushd /opt
-rm -rf /opt/nvim-linux-x86_64 || echo "delete old failed" && return 1
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz || echo "download failed" && return 1
-tar -C /opt -xzf nvim-linux-x86_64.tar.gz || echo "extract failed" && return 1
-ln -sf /opt/nvim-linux-x86_64/bin/nvim /usr/local/bin/nvim || echo "link failed" && return 1
+rm -rf /opt/nvim-linux-x86_64 && wait || (echo "delete old failed" && exit 1)
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz && wait && echo "DOWNLOADED" || (echo "download failed" && exit 1)
+tar -C /opt -xzf nvim-linux-x86_64.tar.gz && wait && echo "Extracted" || ( echo "extract failed" && exit 1 )
+ln -sf /opt/nvim-linux-x86_64/bin/nvim /usr/local/bin/nvim && wait && echo "Linked" || ( echo "link failed" && exit 1)
 popd
 
 export EDITOR=nvim
